97bc1e6f347e006e0e80491ea392f5a03df59cd2
Saitej2456 <saitej2456@gmail.com>
2025-02-24 22:24:41 +0530

    added the feature which will check if all the symbols used are valid or not

--- code_implementation/rtnc.c
+++ code_implementation/rtnc.c
@@ -76,7 +76,7 @@ void printStack(node * top)
     }
 }
 
-void stack(char * regex)
+void paranthesis_check(char * regex)
 {
     node * top = NULL;
     int i = 0;
@@ -85,33 +85,89 @@ void stack(char * regex)
         if((regex[i]=='(')||(regex[i]==')'))
         {
             top = push(top,regex[i]);
-            printStack(top);
+
+            //tester 
+            //used to understand how the stack is after pushing a value into it
+            if(0)
+            {
+                printStack(top);
+                printf("\n");
+            }
+
             if((top->next != NULL)&&(top->data == ')')&&(top->next->data == '('))
             {
                 top = pop(&top);
                 top = pop(&top);
+                //tester 
+                //used to understand how the stack is after poping values from it
+                if(0)
+                {
+                    printStack(top);
+                    printf("\n");
+                }
             }
-            printf("\n");
         }
          
          i++;
     }
     if(isEmpty(top)==1)
     {
-        printf("valid regex");
+        printf("Valid Regex check1 : Passed");
     }
     else
     {
-        printf("invalid regex");
+        printf("Invalid Regex Error: Check the Parenthesis\n");
+        exit(1);
     }
     
 }
 
+void symbol_check(char * regex)
+{
+    bool flag = false;
+    char valid_symbols[] = {'a','b','+','.','*','E'};
+    int i = 0 ;
+    while(regex[i] != '\0')
+    {
+        if((regex[i] == '(')||(regex[i] == ')'))
+        {
+            i++;
+            continue;
+        }
+        else
+        {
+            for(int j = 0 ;j < 6 ; j++)
+            {
+                if(regex[i] == valid_symbols[j])
+                {
+                    flag = true;
+                    break;
+                }
+            }
 
+            if(flag == true)
+            {   
+                i++;
+                flag = false;
+                continue;
+            }
+            printf("\nInvalid Regex Error: invalid Symbol Found in Regex");
+            printf("\nInvalid Regex symbol found : %c\n", regex[i]);
+            exit(1);
+        }
+    }
+    printf("\nValid Regex check2 : Passed\n");
+}
+
+void validation_of_regex(char * regex)
+{
+    paranthesis_check(regex); 
+    symbol_check(regex);
+
+}
 int main()
 {
-    char regex[] = "(a|b)*(";
-    stack(regex); 
-
+    char regex[] = "(a+++b)*(E)*";
+    validation_of_regex(regex);
     return 0;
 }
